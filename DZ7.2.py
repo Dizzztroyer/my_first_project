c5d76902de713710a0da8175bc37c2a415ec9284
hambi <hambir1995@gmail.com>
2026-04-08 22:42:28 +0300

    7.2

--- DZ7.2.py
+++ DZ7.2.py
@@ -6,11 +6,11 @@ def correct_sentence(text):
         text += "."
 
     return text
-
-    return text
 assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
 assert correct_sentence("hello") == "Hello.", 'Test2'
 assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
 assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
 assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
 print('ОК')
+#сначала хотел через .title - чепуха получилась, не то, потом через .capitalize, но не прошел 3й тест.
+#прикольно но мало свободного времени
